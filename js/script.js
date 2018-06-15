0553fba047224c0270dd62240d3c0a6b8c8266b2
Kilic ASLAN <39024802+quiiwi@users.noreply.github.com>
2018-06-15 16:33:05 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* // initialisation de l'objet moi
+var moi;
+
+moi = "kilic"; //Valeur de type string
+
+moi = 30; //Valeur de type number
+
+moi = true; //Type Array
+
+moi = 1.05; // Type float
+
+moi = ["Kilic", 30, 12.5, true]; //Boolean
+
+somme = 100000000;
+
+tableau = ["€", "$", "£"];
+
+victoire = true;
+
+if (victoire = true){
+    alert(moi[0] + " a gagné "+ somme + " " + tableau[1]);
+}
+
+console.log("Salut " + moi[0] + " tu as " + moi[1] + " et " + moi[2]);
+
+document.write(moi);
+ */
+
+//-------------------------------------------------------------------------------
+
+/* var a = 1, b = 1, result = a + b, yolo = prompt("1 + 1 =?")
+
+console.log(result);
+
+if(result>1){
+    document.write("trop fort");
+} */
+
+//-------------------------------------------------------------------------------
+
+
+
+var a = 6;
+var b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b % a);
+
+
