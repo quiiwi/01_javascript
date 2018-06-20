d7654e0fe0062a638bb52301cd17241efb1876c8
Kilic ASLAN <39024802+quiiwi@users.noreply.github.com>
2018-06-20 17:03:59 +0200

    c6
    c6

--- js/script8.js
+++ js/script8.js
@@ -1,4 +1,4 @@
-var A, B, C, D, E, F, K;
+var A, B, C, D, E, F, K, R ,S ,W ,X ,Y;
 
 A = [
     {
@@ -6,14 +6,16 @@ A = [
         password:"azerty",
         mail:"quiiwiki@gmail.com",
         stat:"Membre",
-        pouvoir:"ADMIN"
+        pouvoir:"ADMIN",
+        sexe:"GOD"
     },
     {
         pseudo:"Draski",
         password:"1234",
         mail:"kiwikili@hotmail.fr",
         stat:"Membre",
-        pouvoir:"Moins que rien"
+        pouvoir:"Moins que rien",
+        sexe:"homme"
     }
 ];
 
@@ -22,12 +24,31 @@ function myFunction(){
     C = document.getElementById("password").value;
     D = document.getElementById("email").value;
     K = "Membre";
-    Q = "Moins que rien"
+    Q = "Moins que rien";
 
-    A.push({"pseudo":B, "password":C, "mail":D, "stat":K, "pouvoir":Q});
+    var radio = document.getElementsByClassName('sexe');
+    var valeur;
+    for(var i= 0; i < button.legth; i++){
+        if(button[i].checked){
+            valeur = button[i].value;
+            console.log(valeur);
+        }
+    }
+ 
+    /* S = document.getElementsByName("sexe");
+
+    for(var T = 0; T < S.length; T++){
+        if(S[T].checked){
+        R = S[T].value;
+        }
+    } */
+
+    A.push({"pseudo":B, "password":C, "mail":D, "stat":K, "pouvoir":Q, "sexe":R});
 
     console.log(A);
 }
+
+
 
 function myFunctionReset(){
     document.getElementById("donnees").reset();
