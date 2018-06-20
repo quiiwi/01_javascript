18a8409a4cd3ae79ef49bb5eb360f610f5edafce
Kilic ASLAN <39024802+quiiwi@users.noreply.github.com>
2018-06-20 15:11:08 +0200

    c5
    inscription/stockage/connextion/status/pouvoir

---
+++ js/script10.js
@@ -0,0 +1,13 @@
+var a= [
+    {
+        prénom:"kilic",
+        nom:"aslan",
+        âge:"33"
+    },
+    {
+        prénom:"bakary",
+        nom:"diakite",
+        âge:"21"
+    }
+]
+
--- js/script8.js
+++ js/script8.js
@@ -1,17 +1,19 @@
-var A, B, C, D, E, F;
+var A, B, C, D, E, F, K;
 
 A = [
     {
         pseudo:"Quiiwi",
         password:"azerty",
         mail:"quiiwiki@gmail.com",
-        stat:"ADMIN"
+        stat:"Membre",
+        pouvoir:"ADMIN"
     },
     {
         pseudo:"Draski",
         password:"1234",
         mail:"kiwikili@hotmail.fr",
-        stat:"Membre"
+        stat:"Membre",
+        pouvoir:"Moins que rien"
     }
 ];
 
@@ -20,8 +22,9 @@ function myFunction(){
     C = document.getElementById("password").value;
     D = document.getElementById("email").value;
     K = "Membre";
+    Q = "Moins que rien"
 
-    A.push({"pseudo":B, "password":C, "mail":D, "stat":K});
+    A.push({"pseudo":B, "password":C, "mail":D, "stat":K, "pouvoir":Q});
 
     console.log(A);
 }
@@ -30,7 +33,7 @@ function myFunctionReset(){
     document.getElementById("donnees").reset();
 }
 
-function myFunctionConnexion(){
+/* function myFunctionConnexion(){
 
     var G = prompt(" Tappez votre pseudo");
     if(G === "Quiiwi"){
@@ -41,5 +44,43 @@ function myFunctionConnexion(){
             var J = document.getElementById("god");
             J.innerHTML = "ADMIN / GOD";
         }
-    } 
+    }else if(G === "Draski"){
+        var H = prompt(" Tappez votre mot de passe")
+        if(H === "1234"){
+            var I = document.getElementById("origine");
+            I.innerHTML = "Membre";
+        }
+    }
+} */
+function myFunctionConnexion(){
+
+    var I = document.getElementById("origine");
+    I.innerHTML = "Visiteur";
+    var J = document.getElementById("god");
+    J.innerHTML = "Moins que rien";
+
+
+    var G = prompt(" Tappez votre pseudo");
+    for(var L = 0; L < A.length; L++){
+        if(A[L].pseudo === G){
+            var H = prompt(" Tappez votre mot de passe")
+            for(var O = 0; O < A.length; O++){
+                if(A[L].password === H){
+                    for(var P = 0; P < A.length; P++){
+                        if(A[L].pouvoir === "ADMIN"){
+                            var I = document.getElementById("origine");
+                            I.innerHTML = "Membre";
+                            var J = document.getElementById("god");
+                            J.innerHTML = "ADMIN / GOD";
+                        }else {
+                            var I = document.getElementById("origine");
+                            I.innerHTML = "Membre";
+                            var J = document.getElementById("god");
+                            J.innerHTML = "Moins que rien";
+                        }
+                    } 
+                }
+            }
+        }
+    }
 }
